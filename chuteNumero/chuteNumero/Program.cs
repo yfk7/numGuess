0bdae7cb582311566806c6508d8a43b287bfda3c
Yago Kawauchi <134346973+yfk7@users.noreply.github.com>
2024-06-13 18:43:29 -0300

    Add files via upload

---
+++ chuteNumero/chuteNumero/Program.cs
@@ -0,0 +1,27 @@
+﻿Random rnd = new Random();
+int numeroSecreto = rnd.Next(1, 6);
+
+Console.WriteLine(numeroSecreto);
+do
+{
+    Console.WriteLine("Digite um número de 1 a 5)");
+    int chute = int.Parse(Console.ReadLine()!);
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Você acertou");
+        break;
+    }
+
+    else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("o número é maior");
+    }
+
+    else if(chute > numeroSecreto)
+    {
+        Console.WriteLine("o número é menor");
+    }
+
+} while (true);
+
